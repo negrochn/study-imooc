36f138eb888b6a059354aa9dd1c75cfb591dfba4
negrochn <negrochn7@gmail.com>
2020-08-05 20:21:23 +0800

    2-5 什么是面向对象

--- 255/code/es6/src/index.js
+++ 255/code/es6/src/index.js
@@ -1,11 +1,21 @@
 class Person {
-  constructor(name) {
+  constructor(name, age) {
     this.name = name
+    this.age = age
   }
-  getName() {
-    return this.name
+  eat() {
+    console.log(`${this.name} eat something.`)
+  }
+  speak() {
+    console.log(`My name is ${this.name}, ${this.age} years old.`)
   }
 }
 
-const p = new Person('negrochn')
-console.log(p.getName())
+// 创建实例
+const p1 = new Person('negrochn', 18)
+p1.eat() // negrochn eat something.
+p1.speak() // My name is negrochn, 18 years old.
+
+const p2 = new Person('lexiaodai', 17)
+p2.eat()
+p2.speak()
