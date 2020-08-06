3005e20167e0167971c4f1ddfdc4307bdf9c02cc
negrochn <negrochn7@gmail.com>
2020-08-06 20:50:46 +0800

    2-9 面向对象 - 应用举例

--- 255/code/es6/src/index.js
+++ 255/code/es6/src/index.js
@@ -1,36 +1,30 @@
-class Person {
-  constructor(name, age) {
-    this.name = name
-    this.age = age
+class jQuery {
+  constructor(selector) {
+    const slice = Array.prototype.slice
+    let dom = slice.call(document.querySelectorAll(selector))
+    const len = dom ? dom.length : 0
+    for (let i = 0; i < len; i++) {
+      this[i] = dom[i]
+    }
+    this.length = len
+    this.selector = selector || ''
   }
-  speak() {
-    console.log(`My name is ${this.name}, ${this.age} years old.`)
+  append(node) {
+    // ...
   }
+  addClass(className) {
+    // ...
+  }
+  html(data) {
+    // ...
+  }
+  // 此处省略若干 API
 }
 
-// 子类继承父类
-class Student extends Person {
-  constructor(name, age, studentId) {
-    super(name, age)
-    this.studentId = studentId
-  }
-  speak() {
-    console.log(`My student id is ${this.studentId}.`)
-  }
+window.$ = function(selector) {
+  return new jQuery(selector)
 }
 
-class Teacher extends Person {
-  constructor(name, age, subject) {
-    super(name, age)
-    this.subject = subject
-  }
-  speak() {
-    console.log(`I'am a ${this.subject} teacher.`)
-  }
-}
-
-const s1 = new Student('negrochn', 18, '02954')
-s1.speak() // My student id is 02954.
-const t1 = new Teacher('lexiaodai', 17, 'english')
-t1.speak() // I'am a english teacher.
-
+const ps = $('p')
+console.log(ps)
+console.log(ps.addClass)
