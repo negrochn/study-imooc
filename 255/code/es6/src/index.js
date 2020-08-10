216ec9f491f82e22723614082f564bf2d7d9f15e
negrochn <negrochn7@gmail.com>
2020-08-10 14:22:59 +0800

    3-9 面试真题 1 - 解答

--- 255/code/es6/src/index.js
+++ 255/code/es6/src/index.js
@@ -1,25 +1,37 @@
-function loadImg(src) {
-  return new Promise((resolve, reject) => {
-    const img = document.createElement('img')
-    img.onload = function() {
-      resolve(img)
-    }
-    img.onerror = function() {
-      reject('图片加载失败')
-    }
-    img.src = src
-  })
+class Car {
+  constructor(plateNumber, name) {
+    this.plateNumber = plateNumber
+    this.name = name
+  }
 }
 
-const url = 'https://img.mukewang.com/szimg/5bc16811000192b219201080.jpg'
-const result = loadImg(url)
+class Kuaiche extends Car {
+  constructor(plateNumber, name) {
+    super(plateNumber, name)
+    this.price = 1
+  }
+}
 
-result.then(img => {
-  console.log(img.width)
-  return img
-}).then(img => {
-  console.log(img.height)
-  return img
-}).catch(err => {
-  console.error(err)
-})
+class Zhuanche extends Car {
+  constructor(plateNumber, name) {
+    super(plateNumber, name)
+    this.price = 2
+  }
+}
+
+class Trip {
+  constructor(car) {
+    this.car = car
+  }
+  start() {
+    console.log(`行程开始，车牌号为${this.car.plateNumber}，名称为${this.car.name}。`)
+  }
+  end() {
+    console.log(`行程结束，总价为${this.car.price * 5}`)
+  }
+}
+
+const car = new Kuaiche('浙A·02954', '比亚迪-汉')
+const trip = new Trip(car)
+trip.start() // 行程开始，车牌号为浙A·02954，名称为比亚迪-汉。
+trip.end() // 行程结束，总价为5
