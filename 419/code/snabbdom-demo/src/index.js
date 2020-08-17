066120e5a0f328f40b26e41be470bea3809c3b6c
chenjie <j.chen@hzgosun.com>
2020-08-17 15:47:11 +0800

    更新 Vue 虚拟 DOM 和 diff 算法.md

--- 419/code/snabbdom-demo/src/index.js
+++ 419/code/snabbdom-demo/src/index.js
@@ -1,11 +1,14 @@
-const snabbdom = window.snabbdom
+const { classModule } = snabbdom_class
+const { propsModule } = snabbdom_props
+const { styleModule } = snabbdom_style
+const { eventListenersModule } = snabbdom_eventlisteners
 
 // 定义 patch
 const patch = snabbdom.init([
-  snabbdom_class,
-  snabbdom_props,
-  snabbdom_style,
-  snabbdom_eventlisteners
+  classModule,
+  propsModule,
+  styleModule,
+  eventListenersModule
 ])
 
 // 定义 h
@@ -14,21 +17,30 @@ const h = snabbdom.h
 const container = document.getElementById('container')
 
 // 生成 vnode
-let vnode = h('ul#list', {}, [
-  h('li.item', {}, 'Item 1'),
-  h('li.item', {}, 'Item 2')
+let vnode = h('div#container.two.classes', { on: { click: someFn } }, [
+  h('span', { style: { fontWeight: 'bold' } }, 'This is bold'),
+  ' and this is just normal text',
+  h('a', { props: { href: '/foo' } }, 'I\'ll take you places!')
 ])
 
 patch(container, vnode)
 
 document.getElementById('btn').addEventListener('click', () => {
   // 生成 newVnode
-  const newVnode = h('ul#list', {}, [
-    h('li.item', {}, 'Item 1'),
-    h('li.item', {}, 'Item B'),
-    h('li.item', {}, 'Item 3')
+  const newVnode = h('div#container.two.classes', { on: { click: anotherEventHandler } }, [
+    h('span', { style: { fontWeight: 'normal', fontStyle: 'italic' } }, 'This is now italic type'),
+    ' and this is still just normal text',
+    h('a', { props: { href: '/bar' } }, 'I\'ll take you places!')
   ])
 
   patch(vnode, newVnode)
   vnode = newVnode
 })
+
+function someFn() {
+  console.log('some fn')
+}
+
+function anotherEventHandler() {
+  console.log('another')
+}
