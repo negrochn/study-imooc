a143bc2ee7246a211787f0e0dea519cd9dfbbb3c
chenjie <j.chen@hzgosun.com>
2020-08-17 11:08:13 +0800

    7-2 装饰器模式 - 场景 1

--- 255/code/es6/src/index.js
+++ 255/code/es6/src/index.js
@@ -1,23 +1,10 @@
-class Circle {
-  draw() {
-    console.log('画一个圆')
-  }
+@testDec
+class Demo {
+
 }
 
-class Decorator {
-  constructor(circle) {
-    this.circle = circle
-  }
-  draw() {
-    this.circle.draw()
-    this.setRedBorder(this.circle)
-  }
-  setRedBorder(circle) {
-    console.log('设置红色边框')
-  }
+function testDec(target) {
+  target.isDec = true
 }
 
-const c = new Circle()
-const d = new Decorator(c)
-
-d.draw()
+console.log(Demo.isDec)
