f4b36e80f631cda119c0c8396ecaeeea68baa4ee
negrochn <negrochn7@gmail.com>
2020-08-17 21:51:30 +0800

    7-3 装饰器模式 - 场景 2

--- 255/code/es6/src/index.js
+++ 255/code/es6/src/index.js
@@ -1,10 +1,22 @@
-@testDec
-class Demo {
-
+class Math {
+  // 装饰方法
+  @log
+  add(a, b) {
+    return a + b
+  }
 }
 
-function testDec(target) {
-  target.isDec = true
+function log(target, name, descriptor) {
+  let oldValue = descriptor.value
+
+  descriptor.value = function() {
+    console.log(`Calling ${name} with`, arguments)
+    return oldValue.apply(this, arguments)
+  }
+
+  return descriptor
 }
 
-console.log(Demo.isDec)
+const math = new Math()
+const result = math.add(1, 3) // 会打印日志
+console.log(result)
