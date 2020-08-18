1f2f0f9cc7c66a9b59b27253f94d8fc58ef577c5
negrochn <negrochn7@gmail.com>
2020-08-18 21:44:45 +0800

    8-2 代理模式 - 场景 1

--- 255/code/es6/src/index.js
+++ 255/code/es6/src/index.js
@@ -1,25 +1,7 @@
-class RealImg {
-  constructor(fileName) {
-    this.fileName = fileName
-    this.loadFromDisk() // 初始化即从硬盘加载，模拟
-  }
-  loadFromDisk() {
-    console.log(this.fileName, 'loading...')
-  }
-  display() {
-    console.log('display', this.fileName)
-  }
-}
-
-class ProxyImg {
-  constructor(fileName) {
-    this.realImg = new RealImg(fileName)
-  }
-  display() {
-    this.realImg.display()
-  }
-}
-
-// 测试
-let proxyImg = new ProxyImg('1.png')
-proxyImg.display()
+// 推荐使用 $.proxy 解决，这样就少定义一个变量
+$('#div1').click(function() {
+  setTimeout($.proxy(function() {
+    // this 符合预期
+    $(this).css('background-color', 'yellow')
+  }, this), 1000)
+})
