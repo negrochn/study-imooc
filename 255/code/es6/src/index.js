d2e8105ec24b31620aa0e23cdb3800e244d94123
negrochn <negrochn7@gmail.com>
2020-08-21 17:14:06 +0800

    11-2 迭代器模式 - 演示

--- 255/code/es6/src/index.js
+++ 255/code/es6/src/index.js
@@ -1,14 +1,35 @@
-const arr = [1, 2, 3, 4, 5]
-const nodeList = document.getElementsByTagName('a')
-const $a = $('a')
-
-function each(data) {
-  const $data = $(data)
-  $data.each((key, val) => {
-    console.log(key, val)
-  })
+class Iterator {
+  constructor(container) {
+    this.list = container.list
+    this.index = 0
+  }
+  next() {
+    if (this.hasNext()) {
+      return this.list[this.index++]
+    }
+    return null
+  }
+  hasNext() {
+    if (this.index >= this.list.length) {
+      return false
+    }
+    return true
+  }
 }
 
-each(arr)
-each(nodeList)
-each($a)
+class Container {
+  constructor(list) {
+    this.list = list
+  }
+  // 生成遍历器
+  getIterator() {
+    return new Iterator(this)
+  }
+}
+
+const arr = [1, 2, 3, 4, 5]
+const c = new Container(arr)
+const i = c.getIterator()
+while (i.hasNext()) {
+  console.log(i.next())
+}
