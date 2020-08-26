cbe8aad923a4ce1d106b0231e7525ef619e55ee3
negrochn <negrochn7@gmail.com>
2020-08-26 21:50:42 +0800

    11-4 迭代器模式 - 场景 2

--- 255/code/es6/src/index.js
+++ 255/code/es6/src/index.js
@@ -1,35 +1,12 @@
-class Iterator {
-  constructor(container) {
-    this.list = container.list
-    this.index = 0
-  }
-  next() {
-    if (this.hasNext()) {
-      return this.list[this.index++]
-    }
-    return null
-  }
-  hasNext() {
-    if (this.index >= this.list.length) {
-      return false
-    }
-    return true
+// Symbol.iterator 并不是人人都知道
+// 也不是每个人都需要封装一个 each 函数
+// 因此就有了 for ... of 语法
+function each(data) {
+  for(let item of data) {
+    console.log(item)
   }
 }
 
-class Container {
-  constructor(list) {
-    this.list = list
-  }
-  // 生成遍历器
-  getIterator() {
-    return new Iterator(this)
-  }
-}
-
-const arr = [1, 2, 3, 4, 5]
-const c = new Container(arr)
-const i = c.getIterator()
-while (i.hasNext()) {
-  console.log(i.next())
-}
+each([1, 2, 3, 4, 5])
+each($('a'))
+each(document.querySelectorAll('a'))
