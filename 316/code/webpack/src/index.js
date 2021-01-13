5a07920cdd76c0d64c2f538a0e6f4f822f031364
negrochn <negrochn7@gmail.com>
2021-01-13 16:20:37 +0800

    Tree Shaking 概念详解，development 和 production 模式的区分打包

--- 316/code/webpack/src/index.js
+++ 316/code/webpack/src/index.js
@@ -1,12 +1,3 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import { add } from './math'
 
-class App extends Component {
-  render() {
-    return (
-      <div>Hello World</div>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+console.log(add(1, 2))
